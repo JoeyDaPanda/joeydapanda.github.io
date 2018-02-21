b79f412efced4819c32733496e7cfac739574141
ZacharyMacKenzie <36342885+ZacharyMacKenzie@users.noreply.github.com>
2018-02-21 10:47:27 -0500

    Update Profiles.php

--- pages/Profiles.php
+++ pages/Profiles.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
 
+<html>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+
+  <title>PROPAGANDA</title>
+  <link rel="icon" href="assets/icon.png">
+  <link rel="stylesheet" href="joeydapanda.github.io/style.css" />
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
+
+</head>
+
+<body>
+
+  <center>
+  <br>
+  <a href="http://steamcommunity.com/profiles/76561198130572111"  class="button setWidth blue"><b>Steam</b></a>
+  <br>
+  <a href="http://steamrep.com/profiles/76561198130572111" class="button setWidth blue"><b>SteamRep</b></a>
+
+<br>
+
+<div class="footer">
+<footer>
+  <p>Posted by: Joey Darisse & Zachary MacKenzie</p>
+  <p>To report bugs please <a href="https://github.com/JoeyDaPanda/joeydapanda.github.io/issues">submit an issue.</a></p>
+</footer>
+</div>
+
+</body>
+</html>
