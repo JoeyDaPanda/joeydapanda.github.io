38fdf91386d9f6a9a4299635f3549184ca01a0bc
ZacharyMacKenzie <36342885+ZacharyMacKenzie@users.noreply.github.com>
2018-04-10 16:45:19 -0400

    Update globalJS.js

--- globalJS.js
+++ globalJS.js
@@ -17,3 +17,119 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Password functions for downloads page
+
+  function passwordBennett() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "bennett") {
+      window.location.replace("https://www.dropbox.com/s/iuyll9z0uv2vqi3/Getting%20Over%20It.zip?dl=1");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+    
+    function passwordBennettMac() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "bennett") {
+      window.location.replace("https://www.dropbox.com/s/ydhogktoxl84lro/Getting%20Over%20It%20Installer.dmg?dl=1");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+
+function passwordBennettAndroid() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "bennett") {
+      window.location.replace("https://www.dropbox.com/s/wpo886qvkffq6z6/com.foddy.gettingover_2017-11-23.apk.zip?dl=1");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+	  
+  function passwordPrison() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "prison") {
+      window.location.replace("https://drive.google.com/uc?export=download&confirm=jqFC&id=1HVN0bY2Mst1YaZiSKiRnlmmPUQ7VtnZf");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+    
+    function passwordPrisonMac() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "prison") {
+      window.location.replace("https://drive.google.com/uc?export=download&confirm=1QSd2uN70m4vAQj6dRmRsDSlZVKUOzZ9e");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+
+  function passwordAdobe() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "bennett") {
+      window.location.replace("https://www.dropbox.com/s/qjvqffuggc4z130/Adobe%20Cracked%20DLLs.zip?dl=1");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
