a5ced080baa50ab5f41731b9897383647076067c
Joey Darisse <25378024+JoeyDaPanda@users.noreply.github.com>
2018-04-14 20:16:02 -0400

    esketit

--- js/passwords.js
+++ js/passwords.js
@@ -117,7 +117,7 @@ if (pass1.toLowerCase()!="password" & testV ==3)
   while (testV < 3) {
    if (!pass1) 
       history.go(-1);
-    if (pass1.toLowerCase() == "skyline") {
+    if (pass1.toLowerCase() == "skylines") {
       window.location.replace("https://megaup.net/2E3A?pt=RvTTsmrL47RXIi6%2BRqtOoOGXVtzVDdltvQ4dW3Gzc80%3D");
   break;
 } 
@@ -138,6 +138,25 @@ if (pass1.toLowerCase()!="password" & testV ==3)
       history.go(-1);
     if (pass1.toLowerCase() == "alva") {
       window.location.replace("https://megaup.net/2qPJ?pt=yJMRG8zWMdSnVj7zBO9RB2agtCV%2BGCvvPKmWx6%2BENiA%3D");
+  break;
+} 
+testV+=1;
+  var pass1 = 
+  prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+  history.go(-1);
+  return " ";
+} 
+
+  function passwordCloneHero() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter The Password','');
+  while (testV < 3) {
+   if (!pass1) 
+      history.go(-1);
+    if (pass1.toLowerCase() == "clone") {
+      window.location.replace("https://www.dropbox.com/s/b5zucj9klwjtkq9/Clone%20Hero.zip?dl=0");
   break;
 } 
 testV+=1;
